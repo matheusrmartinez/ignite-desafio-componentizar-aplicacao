79a38cf5aa29527e76052a797a0f7676c278621e
Matheus Martinez <matheus.rmartinez@gmail.com>
2021-06-17 08:09:21 -0300

    Desafio concluído (Faltam testes)

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,50 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { MovieCard } from '../components/MovieCard';
+import { api } from '../services/api';
+
+interface ContentProps {
+  selectedGenreId: number;
+  genreData: GenreResponseProps;
+}
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieData {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+
+export function Content({selectedGenreId, genreData}: ContentProps) {
+  const [movies, setMovies] = useState<MovieData[]>([]);
+
+  useEffect(() => {
+    api.get<MovieData[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [selectedGenreId]);
+
+  return (
+  <>
+    <header>
+    <span className="category">Categoria:<span> {genreData.title}</span></span>
+    </header>
+  <main>
+    <div className="movies-list">
+      {movies.map(movie => (
+        <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+      ))}
+    </div>
+  </main>
+ </>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,50 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { Button } from "./Button";
+import { api } from '../services/api';
+
+interface GenreResponseData {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+interface GenreResponseProps {
+  setSelectedGenreId(value: number): void;
+  selectedGenreId: number;
+  setSelectedGenre(value: GenreResponseData) : void;  
+}
+
+export function SideBar({setSelectedGenreId, selectedGenreId, setSelectedGenre}: GenreResponseProps) {
+
+  const [genres, setGenres] = useState<GenreResponseData[]>([]);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  useEffect(() => {
+    api.get<GenreResponseData[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+
+    api.get<GenreResponseData>(`genres/${selectedGenreId}`).then(response => {
+    setSelectedGenre(response.data);
+    }), [selectedGenreId]});
+
+  return (
+  <nav className="sidebar">
+  <span>Watch<p>Me</p></span>
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>
+  )
+  
 }
